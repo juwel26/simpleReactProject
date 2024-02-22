4e0a47a591b82985374c469c7fa5dc6e9c8a9738
MD. Juwel Rana <rk370613@gmail.com>
2024-02-22 22:39:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react"
+
+
+function App() {
+
+
+  let [counter , setcounter] = useState(0)
+  let [color , setcolor] = useState("white")
+
+//  addfunc
+  function counterAdd(){
+
+  if(counter >=0 && counter < 20){
+    
+    let cancel = document.getElementById("cancel")
+    cancel.style.display = "block"
+     setcounter(counter+1)
+  }else{
+   window.alert("you rich high lavel of number please drecress" )
+   let add = document.getElementById("add")
+   add.style.display = "none"
+  }
+
+  }
+// cancelfunc
+  function counterCancel(){
+
+  if(counter > 0 && counter <= 20){
+    let add = document.getElementById("add")
+    add.style.display = "block"
+    setcounter(counter-= 1)
+  }else{
+   window.alert("you rich low lavel of number please incress" )
+   let cancel = document.getElementById("cancel")
+   cancel.style.display = "none"
+  }
+      
+  }
+
+
+
+  return (
+  
+     <>
+     
+    <div className=" h-auto m-5 shadow-2xl p-5 w-12/12 text-white flex flex-wrap justify-center "style={{background:color}} > 
+
+    <div onClick={()=> setcolor("red")}  className="colorbtn1 m-2 px-4 py-2 bg-red-600 rounded-lg      transition ease-in-out ">red</div>
+    <div onClick={()=> setcolor("cyan")} className="colorbtn2 m-2 px-4 py-2 bg-cyan-600 rounded-lg     transition ease-in-out ">cyan</div>
+    <div onClick={()=> setcolor("yellow")} className="colorbtn3 m-2 px-4 py-2 bg-yellow-600 rounded-lg transition ease-in-out ">yellow</div>
+    <div onClick={()=> setcolor("indigo")} className="colorbtn3 m-2 px-4 py-2 bg-indigo-600 rounded-lg transition ease-in-out ">indigo</div>
+    
+    
+     </div>
+
+
+      <div className =" flex justify-between h-auto w-11/12 md:w-12/12 bg-indigo-500 rounded-md   shadow-xl text-center m-3 p-5 text-xl font-semibold text-white ">
+       <h1 className="font-semibold ">counter : {counter}</h1>
+     <button className=" bg-green-500 m-2 p-4 rounded-md" id="add" onClick={ counterAdd}> add</button>
+       <br />
+     <button className=" ease-out duration-300 bg-red-500  m-2 p-4 rounded-md"  id="cancel" onClick={counterCancel}> cancel </button>
+     </div>
+
+
+
+     
+     
+     
+     
+     </>
+  )
+}
+
+export default App
